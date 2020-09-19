bf659fc6ab4b6759580f380beadcc955da8db571
Dani Alvarez <danidask@gmail.com>
2020-09-19 11:03:26 +0100

    template

---
+++ kicad_tools/kicad_tools.py
@@ -0,0 +1,5 @@
+def main():
+    print("entry point")
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from kicad_tools import __version__
+
+setup(
+    name='kicad2jlcpcb',
+    version=__version__,
+    description='Plugin for KiCad 5 that allows generate files for jlcpcb pcb manufacturing and assembly service',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/danidask/kicad2jlcpcb',
+    author='Daniel Alvarez',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.5',
+    entry_points={
+        "console_scripts": [
+            "kicad2jlcpcb = kicad2jlcpcb.kicad2jlcpcb:main",
+        ]
+    }
+)
